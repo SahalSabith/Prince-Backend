5d63d5415281f97219360e971618eec06b5a2dea
SahalSabith <sahalsabith111@gmail.com>
2025-06-15 19:08:26 +0530

    product issue

--- prince/products/views.py
+++ prince/products/views.py
@@ -24,7 +24,7 @@ class CategoriesListView(APIView):
 
 class ProductsListView(APIView):
     def get(self, request):
-        products = Products.objects.all()
+        products = Product.objects.all()
         serializer = productserializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
