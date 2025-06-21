55b003d87727394bb06b72304507e3cf5f7d12e4
SahalSabith <sahalsabith111@gmail.com>
2025-06-21 21:09:54 +0530

    is popular added

--- prince/products/models.py
+++ prince/products/models.py
@@ -11,6 +11,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='uploads/images/', null=True, blank=True)
+    is_popular = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
